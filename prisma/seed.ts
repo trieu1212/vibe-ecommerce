c373934eb768e79d2badf82c218a848fbc4d1034
qtriu <qtriu@qtrius-MacBook-Pro.local>
2026-01-20 14:36:07 +0700

    fix db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
 import bcrypt from "bcryptjs";
 
 async function getPrismaClient() {
-  const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL || "file:./dev.db",
-  });
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 }
 
 async function main() {
